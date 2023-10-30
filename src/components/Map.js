aae3a9bcccbe0b96fe518f6f69f63475f16f3e9b
Philip Nguyen <100167552+Nguyen-Philip@users.noreply.github.com>
2023-10-29 20:14:18 -0400

    Add files via upload

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import GoogleMapReact from 'google-map-react'
-import { LocationVolcanoMarker, LocationStormMarker, LocationWildfireMarker } from './LocationMarker'
+//import { LocationVolcanoMarker, LocationStormMarker, LocationWildfireMarker } from './LocationMarker'
+import volcanoIcon from './volcano.png'
+import stormIcon from './storm.png'
+import wildfireIcon from './wildfire.png'
 
 const Map = ({ eventData, center, zoom }) => {
-  const [showVolcanoMarkers, setShowVolcanoMarkers] = useState(false)
+  const [showVolcanoMarkers, setShowVolcanoMarkers] = useState(true)
   const [showStormMarkers, setShowStormMarkers] = useState(true)
-  const [showWildfireMarkers, setShowWildfireMarkers] = useState(false)
+  const [showWildfireMarkers, setShowWildfireMarkers] = useState(true)
 
+  /*
   const volcanoMarkers = eventData
     .filter(eventData => showVolcanoMarkers && eventData.categories[0].id === "volcanoes")
     .map(eventData => (
@@ -36,6 +40,75 @@ const Map = ({ eventData, center, zoom }) => {
         lng = { eventData.geometry[0].coordinates[0] }
       />
     ))
+  */
+
+  const [map, setMap] = useState(null)
+
+  useEffect(() => {
+    if (map) {
+      if (showVolcanoMarkers) {
+        eventData
+          .filter(eventData => eventData.categories[0].id === "volcanoes")
+          .forEach(eventData => {
+            const marker = new window.google.maps.Marker({
+              position: {
+                lat: eventData.geometry[0].coordinates[1],
+                lng: eventData.geometry[0].coordinates[0],
+              },
+              map: map,
+              icon: {
+                url: volcanoIcon, // Use the icon URL for volcanoes
+                scaledSize: new window.google.maps.Size(40, 40), // Adjust the size if needed
+              },
+            })
+          })
+      }
+    }
+  }, [map, showVolcanoMarkers, eventData])
+
+  useEffect(() => {
+    if (map) {
+      if (showStormMarkers) {
+        eventData
+          .filter(eventData => eventData.categories[0].id === "severeStorms")
+          .forEach(eventData => {
+            const marker = new window.google.maps.Marker({
+              position: {
+                lat: eventData.geometry[0].coordinates[1],
+                lng: eventData.geometry[0].coordinates[0],
+              },
+              map: map,
+              icon: {
+                url: stormIcon, // Use the icon URL for volcanoes
+                scaledSize: new window.google.maps.Size(40, 40), // Adjust the size if needed
+              },
+            })
+          })
+      }
+    }
+  }, [map, showStormMarkers, eventData])
+
+  useEffect(() => {
+    if (map) {
+      if (showWildfireMarkers) {
+        eventData
+          .filter(eventData => eventData.categories[0].id === "wildfires")
+          .forEach(eventData => {
+            const marker = new window.google.maps.Marker({
+              position: {
+                lat: eventData.geometry[0].coordinates[1],
+                lng: eventData.geometry[0].coordinates[0],
+              },
+              map: map,
+              icon: {
+                url: wildfireIcon, // Use the icon URL for volcanoes
+                scaledSize: new window.google.maps.Size(40, 40), // Adjust the size if needed
+              },
+            })
+          })
+      }
+    }
+  }, [map, showWildfireMarkers, eventData])
 
   return (
     <div className = "map">
@@ -46,10 +119,8 @@ const Map = ({ eventData, center, zoom }) => {
         bootstrapURLKeys = {{ key: 'AIzaSyDymZ1ilLgyS4CpP4psnkpvQ5ZziEJSLjU'}}
         defaultCenter = {center}
         defaultZoom = {zoom}
+        onGoogleApiLoaded={({ map }) => setMap(map)}
       >
-        { volcanoMarkers }
-        { stormMarkers }
-        { wildfireMarkers }
       </GoogleMapReact>
     </div>
   )
@@ -58,7 +129,7 @@ const Map = ({ eventData, center, zoom }) => {
 Map.defaultProps = {
   center: {
     lat: 0,
-    lng: 0
+    lng: 0 
   },
   zoom: 0
 }
