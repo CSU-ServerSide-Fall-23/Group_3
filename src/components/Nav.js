d79d59df665aaa445756962368442291af9cf145
Philip Nguyen <100167552+Nguyen-Philip@users.noreply.github.com>
2023-11-06 14:06:21 -0500

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,22 +5,22 @@ const Nav = () => {
         <nav class = "navbar navbar-expand-lg bg-body-tertiary nav-fill">
         <div class = "container-fluid">
             <a class = "navbar-brand">
-                <img src = 'https://media.tenor.com/LYftKBBe2csAAAAi/earth-planet.gif' height = {30} width = {30} />
+                <img src = 'https://media.tenor.com/LYftKBBe2csAAAAi/earth-planet.gif' alt = 'planet earth spinning gif' height = {30} width = {30} />
             </a>
 
             <div class = "collapse navbar-collapse btn-3" id = "navbarNav">
             <ul class = "navbar-nav">
                 <li class = "nav-item">
-                    <a class = "nav-link active fw-bold" href = "https://github.com/takapia95/Group_3/tree/main">TrackMe</a>
+                    <a class = "nav-link active fw-bold" href = "https://github.com/CSU-ServerSide-Fall-23/Group_3">TrackMe</a>
                 </li>
                 <li class = "nav-item">
                     <a class = "nav-link active fw-bold" href = "https://api.nasa.gov/">NASA APIs</a>
                 </li>
                 <li class = "nav-item">
-                    <a class = "nav-link active fw-bold" href = "https://eonet.gsfc.nasa.gov/docs/v3">EONET</a>
+                    <a class = "nav-link active fw-bold" href = "https://eonet.gsfc.nasa.gov/docs/v3">EONET API</a>
                 </li>
             </ul>
-            <h4>Natural disaster tracker</h4>
+              <h4>Natural Disaster tracker</h4>
             </div>
         </div>
         </nav>
