39f9414b67530fce021726b3b96bfd9138df4551
takapia95 <113641497+takapia95@users.noreply.github.com>
2023-11-18 02:36:04 -0500

    Update LocationInfo.js try this to see the close button

--- src/components/LocationInfo.js
+++ src/components/LocationInfo.js
@@ -24,3 +24,63 @@ const LocationInfo = ({ info }) => {
 }
 
 export default LocationInfo
+/*
+  import React, { useState } from 'react';
+
+const LocationInfo = ({ info, onClose, isVisible, toggleVisibility }) => {
+  return isVisible ? (
+    <div className="location-info">
+      <button type="button" className="btn-close" aria-label="Close" onClick={onClose}/>
+ 
+      <h2>Event Information</h2>
+      <ul>
+        <li>
+          TYPE: <strong>{info.type}</strong>
+        </li>
+        <li>
+          NAME: <strong>{info.title}</strong>
+        </li>
+        <li>
+          EONET API ID: <strong>{info.id}</strong>
+        </li>
+        <li>
+          CORDINATES: <strong>{info.coordinates}</strong>
+        </li>
+        <li>
+          ADDITIONAL INFORMATION:{' '}
+          <strong>
+            <a href={info.link} target="_blank" rel="noopener noreferrer">
+              {info.link}
+            </a>
+          </strong>
+        </li>
+      </ul>
+    </div>
+  ) : (
+    <button type="button" onClick={toggleVisibility}>
+      Show Location Info
+    </button>
+  );
+};
+
+const LocationInfoContainer = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div>
+      <LocationInfo
+        info={{ type: 'Type', title: 'Title', id: 'ID', coordinates: 'Coordinates', link: 'Link' }}
+        onClose={toggleVisibility}
+        isVisible={isVisible}
+        toggleVisibility={toggleVisibility}
+      />
+    </div>
+  );
+};
+
+export default LocationInfoContainer;
+*/
