6268641e1d1799340a22b1f6dd4f6a30d7fc4cc7
Philip Nguyen <100167552+Nguyen-Philip@users.noreply.github.com>
2023-12-02 20:11:27 -0500

    Update MapButtons.js

--- src/components/MapButtons.js
+++ src/components/MapButtons.js
@@ -7,7 +7,7 @@ import { MdLocalFireDepartment } from 'react-icons/md'
 function MapButtons({ toggleVolcanoMarkers, toggleStormMarkers, toggleWildfireMarkers }) 
 {
   return (
-    <FloatButton.Group shape = 'square' style = {{ right: 19, bottom: 200 }}>
+    <FloatButton.Group className = "map-buttons" shape = 'square' style = {{ right: 19, bottom: 200 }}>
     <FloatButton icon = { <MdVolcano /> }onClick = { toggleVolcanoMarkers } tooltip = 'Volcanoes'/>
     <FloatButton icon = { <MdOutlineStorm /> }onClick = { toggleStormMarkers } tooltip = 'Storms'/>
     <FloatButton icon = { <MdLocalFireDepartment /> }onClick = { toggleWildfireMarkers } tooltip = 'Wildfires'/>
